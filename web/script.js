7fcc49ea8caf65f742510f529e0e86e7c618f466
nho2966-dot <nho2966@gmail.com>
2026-01-01 09:46:40 +0400

    Delete web directory

--- web/script.js
+++
@@ -1,4 +0,0 @@
-fetch('../logs/bot.log')
-  .then(res => res.text())
-  .then(data => document.getElementById('log-content').textContent = data || 'لا توجد سجلات.')
-  .catch(err => document.getElementById('log-content').textContent = '❌ خطأ: ' + err.message);
