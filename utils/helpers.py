917051873a6eaecbfb2975431ba04aee1511e760
nho2966-dot <nho2966@gmail.com>
2026-02-02 11:07:42 +0400

    Add technology news sources to helpers.py
    Added a list of reliable technology news sources.

--- utils/helpers.py
+++ utils/helpers.py
@@ -6,3 +6,10 @@ def is_peak_time(peak_hours):
 
 def choose_post_type():
     return random.choice(["tweet", "thread"])
+SOURCES = [
+    "https://www.theverge.com/rss/index.xml",      # المصدر الأول عالمياً
+    "https://9to5mac.com/feed/",                   # أخبار أبل العاجلة
+    "https://techcrunch.com/feed/",                # أخبار الشركات والشركات الناشئة
+    "https://www.bloomberg.com/feeds/technology/sitemap_news.xml", # أخبار اقتصاد التقنية الموثوقة
+    "https://www.reutersagency.com/feed/?best-topics=technology" # وكالة رويترز (للأخبار المؤكدة 100%)
+]
